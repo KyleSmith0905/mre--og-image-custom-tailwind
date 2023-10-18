f3c43d9cb5cfb204551805511a7987e0989316b1
kylesmith090502 <me@yskkyle.com>
2023-10-18 16:16:20 -0400

    Diverted my minimum reproducible example to something else.

--- components/ogImage/search.vue
+++ components/ogImage/search.vue
@@ -6,9 +6,19 @@ defineProps<{
   title?: string;
 }>();
 </script>
+
 <template>
-  <div class="w-full h-full text-white bg-gradient-to-tr from-green-600 to-green-700">
-		<p class='font-["Unbounded"] text-4xl'>{{title}}</p>
-		<p class='text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, ipsum veniam nobis voluptatem fugit rerum sunt officiis, explicabo at eos corporis. Natus modi sunt quisquam veniam assumenda quam aspernatur corrupti?</p>
+  <div class='bg-white'>
+		<h1>{{title}}</h1>
+		<p class='some-text-element'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, ipsum veniam nobis voluptatem fugit rerum sunt officiis, explicabo at eos corporis. Natus modi sunt quisquam veniam assumenda quam aspernatur corrupti?</p>
   </div>
 </template>
+
+<style scoped lang="scss">
+h1 {
+	color: blue;
+}
+.some-text-element {
+  color: red;
+}
+</style>
